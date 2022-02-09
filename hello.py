f259f0619c1aeaf43e4dd0787b709f71b40baa2d
Tayasui Su <syjsu@qq.com>
2022-02-09 14:13:52 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,2 @@
-print("Hello World")
-
-with open("test_data.json", 'w', encoding="utf-8") as f:
-    f.write("[1,2,3,4,5]")
+with open("api.json", 'w', encoding="utf-8") as f:
+    f.write("hello")
